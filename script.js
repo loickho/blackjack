51653d6b1173ad299f8e4e5d0ac7150f76ed3a64
loic khodarkovsky <lkhodarkovsky@gmail.com>
2023-09-13 19:13:39 +0200

    added function for creating a new deck of cards

--- script.js
+++ script.js
@@ -1,5 +1,33 @@
-const suits = ['♠️', '♣', '❤️', '♦️']
-const numbers = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'K', 'Q', 'A']
+const suits = ['♠️', '♣', '❤️', '♦️'];
+const numbers = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'K', 'Q', 'A'];
+let deck = [];
 
-console.log(numbers)
-console.log(suits)
+// create deck of cards
+function newDeck(){
+  suits.forEach((suit) =>
+    numbers.forEach((number) =>
+      deck.push(number + suit)
+    )
+  )
+}
+
+newDeck()
+console.log(deck)
+
+// shuffle deck
+
+// deal random card to dealer twice (one facedown)
+
+// deal random card to player twice
+
+// define what happens when hit is clicked
+  // if goes over 21, lose
+  // otherwise, hit again or allow to stay
+
+// define what happens when stay is clicked
+
+// dealer hits again until score is greater than 17
+
+// decide winner
+
+// play again
