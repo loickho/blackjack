15f57976e5541a5ad866bb4a081e5cb552ce69ba
loic khodarkovsky <lkhodarkovsky@gmail.com>
2023-09-13 17:26:22 +0200

    added card varialbes

--- script.js
+++ script.js
@@ -1,1 +1,5 @@
-console.log('hello')
+const suits = ['♠️', '♣', '❤️', '♦️']
+const numbers = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'K', 'Q', 'A']
+
+console.log(numbers)
+console.log(suits)
